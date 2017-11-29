a3ce5827c6151bddc90da4acc29c44d5d600ad0a
Satang1995 <numnim.ps1995@gmail.com>
2017-11-29 10:31:21 +0700

    TestSwtich

--- src/ooplab3/TestSwtich.java
+++ src/ooplab3/TestSwtich.java
@@ -10,17 +10,10 @@ public class TestSwtich {
         System.out.print("Please enter number 1-3: ");
         select = scanner.nextInt();
         switch (select) {
-            case 1:
-                System.out.println("Your entered 1");
-                break;
-            case 2:
-                System.out.println("Your entered 2");
-                break;
-            case 3:
-                System.out.println("Your entered 3");
-                break;
-            default:
-                System.out.println("Number 1-3 only.");
+            case 1: System.out.println("Your entered 1");break;
+            case 2: System.out.println("Your entered 2");break;
+            case 3: System.out.println("Your entered 3");break;
+            default: System.out.println("Number 1-3 only.");
         }//switch
     }//main
 }//class
